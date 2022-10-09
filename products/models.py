57273e824ba7aea2cb80229dbf5e26a6c05466c3
Dustin Li <dustin.li1988@gmail.com>
2022-10-09 21:51:11 +0000

    Add has sizes to product model and size selections to product details page

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     sku = models.CharField(max_length=250, null=True, blank=True)
     description = models.TextField(blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
