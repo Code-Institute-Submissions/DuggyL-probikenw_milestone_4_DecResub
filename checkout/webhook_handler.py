d2d2871f03cef5c35a8705aa3fd9a38e224e3d8a
Dustin Li <dustin.li1988@gmail.com>
2022-10-15 21:11:44 +0000

    Added class methods for webhook handler and vjew

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -9,5 +9,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """Handle unexpected/unknown event"""
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle Stripe payment_intent_succeeded webhook"""
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """Handle Stripe payment_intent_payment_failed webhook"""
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
