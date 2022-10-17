e05854c39b988e5960871b71adf24dcfe9d6f69c
Dustin Li <dustin.li1988@gmail.com>
2022-10-17 00:37:10 +0000

    Fixing deployment issue

--- probikenw/settings.py
+++ probikenw/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '!790bg)uy5_^!j!k&rh=vlyt^j_v1vto&r6=f&n)1s!dhf*-a$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
