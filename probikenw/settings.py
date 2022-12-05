b5bc63d0eaf636a1aece466c164a9658e21b548c
Dustin Li <dustin.li1988@gmail.com>
2022-12-05 22:53:31 +0000

    Setting up debug to development and hide secret key

--- probikenw/settings.py
+++ probikenw/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-50+e^ec79p(=%4wn2m_@zt=t6h36+$^mh63s65j#m2yqe@m)_t'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['probike-nw.herokuapp.com', 'localhost']
 
