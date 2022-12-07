f91df1c549d7900b0816d7a83814c0c0c1fa2525
Dustin Li <dustin.li1988@gmail.com>
2022-12-07 22:51:57 +0000

    Fixed setting bugs

--- probikenw/settings.py
+++ probikenw/settings.py
@@ -15,18 +15,20 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'clear')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['probike-nw.herokuapp.com', 'localhost']
+
 
 # Application definition
 
